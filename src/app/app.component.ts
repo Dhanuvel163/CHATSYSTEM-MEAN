30a05b4d71c8640f87bb1d799419bc1234d6ced5
Dhanavel <dhanuram99@gmail.com>
2022-07-22 22:19:14 +0530

    lap ui done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CHATAPP';
+  title = 'CHAT-IT';
+  constructor(public Router:Router){
+  }
 }
